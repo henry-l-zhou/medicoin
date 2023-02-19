b04da20d88cc6bf85fe2b9ada390e54620acffe6
Jerry Xiang <jerrx221@gmail.com>
2023-02-19 14:30:30 -0600

    commit calendar

---
+++ components/calendar.js
@@ -0,0 +1,19 @@
+import {useState} from 'react';
+import React from 'react';
+import Calendar from 'react-calendar'; 
+import './App.css';
+
+export default function CalendarApp(){
+    const [date, setDate] = useState(new Date())
+
+
+
+    return (
+     <div className="app">
+       <div className="react-calendar, center-screen">
+         <Calendar onChange={setDate} value={date}/>
+       </div>
+     </div>
+      )
+    
+    }
