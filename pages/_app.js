ea28c569c3dc5440c937ad944fdc46dacfeeb4bf
fujiason <fujiason100@gmail.com>
2023-02-18 21:48:35 -0600

    Slider added but not working

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,16 @@
 import '@/styles/globals.css'
 import { SessionProvider } from 'next-auth/react'
+import { TokenContext } from '@/contexts/TokenContext'
+import { useMemo, useState } from 'react'
 
 export default function App({ Component, pageProps, session }) {
+  const [tokenAmount, setTokenAmount] = useState(0)
+
   return (
     <SessionProvider session={session}>
-      <Component {...pageProps}/>
+      <TokenContext.Provider value={{tokenAmount, setTokenAmount}}>
+        <Component {...pageProps}/>
+      </TokenContext.Provider>
     </SessionProvider>
   )
 }
